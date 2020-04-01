21dd392c2808a9be1860fb0d927e6c4229fbc0a5
Lucas Dutra <ldutra98@gmail.com>
2020-04-01 02:19:51 -0300

    Create project initial structure

---
+++ project/__init__.py
@@ -0,0 +1,25 @@
+import os
+from flask import Flask, jsonify
+# from project.api.views import pax_blueprint
+from database_singleton import Singleton
+
+
+# instantiate the app
+def create_app(script_info=None):
+    # Instantiate the app
+    app = Flask(__name__)
+
+    # Set Configuration
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
+
+    db = Singleton().database_connection()
+    migrate = Singleton().migration()
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # register blueprints
+    # app.register_blueprint(pax_blueprint, url_prefix='/pax')
+
+    return app
