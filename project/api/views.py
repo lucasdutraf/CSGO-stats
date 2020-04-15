c9a611635f348fcfb538cf225efc34fa4b64ec33
Lucas Dutra <ldutra98@gmail.com>
2020-04-15 16:54:13 -0300

    Create add kda and adr route

--- project/api/views.py
+++ project/api/views.py
@@ -38,12 +38,55 @@ def add_player():
     except exc.IntegrityError:
         db.session.rollback()
         return jsonify(utils.createFailMessage('DB error')), 400
-# @cs_blueprint.route('/add_kda', methods=['POST'])
-# def add_kda():
 
-# @cs_blueprint.route('/add_adr', methods=['POST'])
-# def add_adr():
+@cs_blueprint.route('/add_kda', methods=['POST'])
+def add_kda():
+    post_data = request.get_json()
+    if not post_data:
+        return jsonify(utils.createFailMessage('Wrong JSON')), 400
+    
+    player = post_data.get('player')
 
+    user_id = player.get('user_id')
+    kda = player.get('kda')
+
+    try:
+        user_kda = KDA(user_id, kda)
+
+        db.session.add(user_kda)
+        db.session.flush()
+        db.session.commit()
+
+        return jsonify(utils.createSuccessMessage('KDA added!')), 201
+
+    except exc.IntegrityError:
+        db.session.rollback()
+        return jsonify(utils.createFailMessage('DB error')), 400
+
+@cs_blueprint.route('/add_adr', methods=['POST'])
+def add_adr():
+    post_data = request.get_json()
+    if not post_data:
+        return jsonify(utils.createFailMessage('Wrong JSON')), 400
+    
+    player = post_data.get('player')
+
+    user_id = player.get('user_id')
+    adr = player.get('adr')
+
+    try:
+        user_adr = ADR(user_id, adr)
+
+        db.session.add(user_adr)
+        db.session.flush()
+        db.session.commit()
+
+        return jsonify(utils.createSuccessMessage('ADR added!')), 201
+
+    except exc.IntegrityError:
+        db.session.rollback()
+        return jsonify(utils.createFailMessage('DB error')), 400
+        
 # @cs_blueprint.route('/rank', methods=['GET'])
 # def get_rank():
 
